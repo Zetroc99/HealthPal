c3ca21e7526f73de93b036b3693ce37297fff26d
Zetroc99 <49223290+Zetroc99@users.noreply.github.com>
2022-08-16 23:57:11 -0700

    more progress

--- mods/load.py
+++ mods/load.py
@@ -3,8 +3,10 @@ import pandas as pd
 
 tree = ET.parse("./data/apple_health_export/export.xml")
 root = tree.getroot()
+BAD_FEATURES = ['sourceVersion', 'device']
 
 
+# TODO drop  sourceVersion, device
 def create_df(record_type: str):
     """
     'Record' or 'Workout'
@@ -25,7 +27,7 @@ def watch_fix(df: pd.DataFrame):
 def clean_record_df(df: pd.DataFrame):
     # to datetime
     for col in ['creationDate', 'startDate', 'endDate']:
-        df[col] = pd.to_datetime(df[col])
+        df[col] = pd.to_datetime(df[col], utc=True)
 
     # value to numeric or nan
     df['value'] = pd.to_numeric(df['value'], errors='coerce')
@@ -38,7 +40,7 @@ def clean_record_df(df: pd.DataFrame):
 
     watch_fix(df)
 
-    return df
+    return df.drop(BAD_FEATURES)
 
 
 def clean_workout_df(df: pd.DataFrame):
@@ -48,7 +50,7 @@ def clean_workout_df(df: pd.DataFrame):
 
     # to datetime
     for col in ['creationDate', 'startDate', 'endDate']:
-        df[col] = pd.to_datetime(df[col])
+        df[col] = pd.to_datetime(df[col], utc=True)
 
     # string to numeric
     df['duration'] = pd.to_numeric(df['duration'])
@@ -57,7 +59,7 @@ def clean_workout_df(df: pd.DataFrame):
 
     watch_fix(df)
 
-    return df
+    return df.drop(BAD_FEATURES)
 
 
 
--- mods/tools.py
+++ mods/tools.py
@@ -1,14 +1,28 @@
 import pandas as pd
 import datetime as dt
+from pandas import DataFrame
+
+FEATURES = ['type', 'value', 'unit', 'creationDate', 'startDate', 'endDate']
+RECORD_TYPES = ['Height', 'BodyMass', 'HeartRate', 'RespiratoryRate',
+                'StepCount',
+                'DistanceWalkingRunning', 'BasalEnergyBurned',
+                'ActiveEnergyBurned', 'FlightsClimbed', 'AppleExerciseTime',
+                'DistanceSwimming', 'SwimmingStrokeCount', 'RestingHeartRate',
+                'VO2Max', 'WalkingHeartRateAverage', 'HeadphoneAudioExposure',
+                'WalkingDoubleSupportPercentage', 'SixMinuteWalkTestDistance',
+                'AppleStandTime', 'WalkingSpeed', 'WalkingStepLength',
+                'WalkingAsymmetryPercentage', 'SleepDurationGoal',
+                'AppleWalkingSteadiness', 'SleepAnalysis', 'AppleStandHour',
+                'MindfulSession', 'HeartRateVariabilitySDNN']
 
 
 # create
-def get_record_type(records, cond):
+def get_record_type(records, cond) -> pd.DataFrame:
     return records[cond]
 
 
 # dates
-def get_range_from_to(df, start, end):  # TODO utc affect range
+def get_range_from_to(df, start, end) -> pd.DataFrame:
     start = pd.to_datetime(start, utc=True)
     end = pd.to_datetime(end, utc=True)
     data = df[df['creationDate'] >= start]
@@ -18,37 +32,42 @@ def get_range_from_to(df, start, end):  # TODO utc affect range
 
 
 # dates
-def get_on_date(df, year: int, month: int, day: int):
+def get_on_date(df, year: int, month: int, day: int) -> pd.DataFrame:
     date = pd.to_datetime(dt.date(year, month, day), utc=True)
     return df[df['creationDate'].dt.date == date]
 
 
 # dates
-def get_on_month_of_year(df, year: int, month: int, day=1):
+def get_on_month_of_year(df, year: int, month: int, day=1) -> pd.DataFrame:
     date = pd.to_datetime(dt.date(year, month, day), utc=True)
     return df[(df['creationDate'].dt.year == date.year) &
               (df['creationDate'].dt.month == date.month)]
 
 
-# TODO all stats have value, date, and unit as important features;  probably want to return series instead of df
-def get_daily_stat(records, year: int, month: int, day: int, match: str):
-    cond = records['type'] == match
-    data = get_record_type(records, cond)
-    daily = get_on_date(data, year, month, day)
-    return daily
+# stats
+def get_daily_stat(records, year: int, month: int, day: int,
+                   match: str) -> DataFrame | None:
+    if match in RECORD_TYPES:
+        cond = records['type'] == match
+        data = get_record_type(records, cond)
+        daily = get_on_date(data, year, month, day)
+        return daily[FEATURES]
+    return None
 
 
-def get_monthly_stat(records, year: int, month: int, match: str):
-    cond = records['type'] == match
-    data = get_record_type(records, cond)
-    monthly = get_on_month_of_year(data, year, month)
-    return monthly
+# stats
+def get_monthly_stat(records, year: int, month: int,
+                     match: str) -> pd.DataFrame | None:
+    if match in RECORD_TYPES:
+        cond = records['type'] == match
+        data = get_record_type(records, cond)
+        monthly = get_on_month_of_year(data, year, month)
+        return monthly[FEATURES]
+    return None
 
 
-# TODO split into their own energy types and make a new total_cal method to
-# TODO keep consistency across get_monthly of all kinds of types
 # calories
-def get_daily_calories(records, year: int, month: int, day: int):
+def get_daily_calories(records, year: int, month: int, day: int) -> float:
     cond = (records['type'] == 'BasalEnergyBurned') | \
            (records['type'] == 'ActiveEnergyBurned')
     calories = get_record_type(records, cond)
@@ -65,27 +84,36 @@ def get_monthly_calories(records, year: int, month: int):
     return total_cal
 
 
+# TODO set workout related functions in a separate file ?
 # workouts
 def get_workout_by_type(df, workout_type: str):
     return df[df['type'] == workout_type]
 
 
-# heart rate
-def get_heartrate_for_date(hr, start, end):
-    hr = hr[hr['startDate'] >= start]
-    hr = hr[hr['endDate'] <= end]
+# workouts
+def get_heartrate_for_workout(records, workout):
+    cond = records['type'] == 'HeartRate'
+    heartrate = get_record_type(records, cond)
 
-    return hr
+    def get_heartrate_for_date(hr, start, end):
+        hr = hr[hr['startDate'] >= start]
+        hr = hr[hr['endDate'] <= end]
 
-
-# heart rates
-def get_heartrate_for_workout(heartrate, workout):
-    assert 'HeartRate' in heartrate['type'].unique(), "Incorrect dataframe, " \
-                                                      "must be Record " \
-                                                      "dataframe "
-
+        return hr
+# workout item must be 1D, use
     return get_heartrate_for_date(heartrate, workout['startDate'].item(),
                                   workout['endDate'].item())
+
+# TODO get workouts on day
+# TODO get workouts on month
+# TODO get heartrate from _ to _ ?
+
+# TODO get values (Records) -> series
+# TODO get duration (Workouts) -> float
+# TODO get totalDistance (Workouts) -> float
+# TODO get totalEnergyBurned (Workouts) -> float
+# TODO merge workout and record tables?
+# TODO split Records types into their own csv files
 
 
 # cleaning
